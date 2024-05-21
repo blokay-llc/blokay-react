886c009d6853123cbffdd2479aeaf8ad0c331e29
judare <fosebadgame@gmail.com>
2024-05-21 08:59:26 -0500

    Export default neuron files

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,6 @@
 import "./index.css";
 import AppButton from "./components/DS/Form/AppButton";
+import Neuron from "./components/Brain/Neuron";
+import NeuronResponse from "./components/Brain/NeuronResponse";
 
-export { AppButton };
+export { AppButton, Neuron, NeuronResponse };
