8a17bc5394fc8d7988affbc589fe692cb434fe60
judare <fosebadgame@gmail.com>
2024-05-21 18:24:38 -0500

    session JWT, setJWT, hook useSession

---
+++ lib/hooks/useSession.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { decodeJWT } from "../helpers/jwt";
+
+export default function useSession() {
+  const [session, setSession] = useState<any>(null);
+
+  const key = "jwt_token_session";
+  const getSession = () => {
+    const token = localStorage.getItem(key);
+    if (token) {
+      const decoded = decodeJWT(token);
+      setSession(decoded);
+    }
+  };
+
+  useEffect(() => {
+    getSession();
+  }, []);
+
+  return session;
+}
--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,27 @@
 import "./index.css";
+
+import useSession from "./hooks/useSession";
 import Neuron from "./components/Brain/Neuron";
 import NeuronResponse from "./components/Brain/NeuronResponse";
 import Events from "./components/Brain/Events";
 import * as DS from "./components/DS/Index";
 
-export { Neuron, Events, NeuronResponse, DS };
+const session = {
+  setJWT(token: string) {
+    localStorage.setItem("jwt_token_session", token);
+  },
+  createSession(data: any) {
+    console.log(data);
+  },
+  useSession,
+  logout() {},
+};
+
+const blocks = {
+  Events,
+  Neuron,
+  NeuronResponse,
+  Block: Neuron,
+};
+
+export default { ...session, ...blocks, DS };
