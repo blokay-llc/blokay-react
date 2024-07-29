d52c2597e26a2cd22ec4b0b6bd39f6bfdff4712b
judare <fosebadgame@gmail.com>
2024-07-29 08:46:01 -0500

    Refactor TableCell component to handle long text display

--- lib/components/Blocks/Types/Table/TableCell.tsx
+++ lib/components/Blocks/Types/Table/TableCell.tsx
@@ -6,37 +6,42 @@ type TableCellProps = {
   showAll: (td: string) => void;
 };
 
+const RenderTd = ({ td, eventsRef, showAll }: TableCellProps) => {
+  if (td == null) {
+    return <strong>NULL</strong>;
+  } else if (td?.type == "money") {
+    return <span>{money(td.text)}</span>;
+  } else if (td?.html) {
+    return (
+      <div
+        onClick={() => {
+          td.click && eventsRef.current.functions[td.click](td.args);
+        }}
+        dangerouslySetInnerHTML={{
+          __html: td.html,
+        }}
+      />
+    );
+  } else if (td?.type == "text") {
+    return <span>{td.text}</span>;
+  }
+
+  if (td?.length > 50) {
+    <div>
+      <div>{("" + td).substring(0, 50)}...</div>
+      <div className="bl-table-show-all" onClick={() => showAll(td)}>
+        Show all
+      </div>
+    </div>;
+  }
+
+  return td;
+};
+
 export function TableCell({ td, eventsRef, showAll }: TableCellProps) {
-  const renderTd = () => {
-    if (td == null) {
-      return <strong>NULL</strong>;
-    } else if (td?.type == "money") {
-      return <span>{money(td.text)}</span>;
-    } else if (td?.html) {
-      return (
-        <div
-          onClick={() => {
-            td.click && eventsRef.current.functions[td.click](td.args);
-          }}
-          dangerouslySetInnerHTML={{
-            __html: td.html,
-          }}
-        />
-      );
-    } else if (td?.type == "text") {
-      return <span>{td.text}</span>;
-    }
-
-    if (td.length > 50) {
-      <div>
-        <div>{("" + td).substring(0, 50)}...</div>
-        <div className="bl-table-show-all" onClick={() => showAll(td)}>
-          Show all
-        </div>
-      </div>;
-    }
-
-    return td;
-  };
-  return <td>{renderTd()}</td>;
+  return (
+    <td>
+      <RenderTd td={td} eventsRef={eventsRef} showAll={showAll} />
+    </td>
+  );
 }
