4760e41ec063dd8f7a8951861854653db3e4f018
judare <fosebadgame@gmail.com>
2024-07-29 08:22:06 -0500

    Refactor TableCell component render logic for readability

--- lib/components/Blocks/Types/Table/TableCell.tsx
+++ lib/components/Blocks/Types/Table/TableCell.tsx
@@ -5,44 +5,38 @@ type TableCellProps = {
   eventsRef: any;
   showAll: (td: string) => void;
 };
+
 export function TableCell({ td, eventsRef, showAll }: TableCellProps) {
-  return (
-    <td>
-      {typeof td == "object" && (
-        <>
-          {td == null && <strong>NULL</strong>}
-          {(td?.type == "money" || td?.type == "number") && (
-            <span>{money(td.text)}</span>
-          )}
-          {td?.type == "text" && <span>{td.text}</span>}
+  const renderTd = () => {
+    if (td == null) {
+      return <strong>NULL</strong>;
+    } else if (td?.type == "money") {
+      return <span>{money(td.text)}</span>;
+    } else if (td?.html) {
+      return (
+        <div
+          onClick={() => {
+            td.click && eventsRef.current.functions[td.click](td.args);
+          }}
+          dangerouslySetInnerHTML={{
+            __html: td.html,
+          }}
+        />
+      );
+    } else if (td?.type == "text") {
+      return <span>{td.text}</span>;
+    }
 
-          {td?.html && (
-            <div
-              onClick={() => {
-                td.click && eventsRef.current.functions[td.click](td.args);
-              }}
-              dangerouslySetInnerHTML={{
-                __html: td.html,
-              }}
-            />
-          )}
-        </>
-      )}
+    if (td.length > 50) {
+      <div>
+        <div>{("" + td).substring(0, 50)}...</div>
+        <div className="bl-table-show-all" onClick={() => showAll(td)}>
+          Show all
+        </div>
+      </div>;
+    }
 
-      {typeof td != "object" && (
-        <>
-          {td.length > 50 ? (
-            <div>
-              <div>{("" + td).substring(0, 50)}...</div>
-              <div className="bl-table-show-all" onClick={() => showAll(td)}>
-                Show all
-              </div>
-            </div>
-          ) : (
-            td
-          )}
-        </>
-      )}
-    </td>
-  );
+    return td;
+  };
+  return <td>{renderTd()}</td>;
 }
